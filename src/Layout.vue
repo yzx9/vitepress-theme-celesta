826b15b41c42a9400ec7aaf04805469e6e3866a3
yzx9 <yuan.zx@outlook.com>
2023-12-13 18:04:27 +0800

    perf: dynamic layout loading

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { useData } from "vitepress"
-import { computed, provide, ref, watch } from "vue"
+import { computed, defineAsyncComponent, provide, ref, watch } from "vue"
 import { Layout, resolveLayout } from "./frontmatter"
 import { KEY_I18N, switchI18n } from "./i18n"
-import About from "./layouts/About.vue"
-import Archives from "./layouts/Archives.vue"
-import Categories from "./layouts/Categories.vue"
-import Home from "./layouts/Home.vue"
-import NotFound from "./layouts/NotFound.vue"
-import Page from "./layouts/Page.vue"
-import Tags from "./layouts/Tags.vue"
+
+const About = defineAsyncComponent(() => import("./layouts/About.vue"))
+const Archives = defineAsyncComponent(() => import("./layouts/Archives.vue"))
+const Categories = defineAsyncComponent(
+  () => import("./layouts/Categories.vue")
+)
+const Home = defineAsyncComponent(() => import("./layouts/Home.vue"))
+const NotFound = defineAsyncComponent(() => import("./layouts/NotFound.vue"))
+const Page = defineAsyncComponent(() => import("./layouts/Page.vue"))
+const Tags = defineAsyncComponent(() => import("./layouts/Tags.vue"))
 
 const { page, frontmatter, localeIndex } = useData()
 
